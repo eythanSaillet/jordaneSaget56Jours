6eddce0cd0c6100519904222d51c940fc0d9a650
eythanSaillet <eythan.saillet@gmail.com>
2020-04-29 18:52:46 +0200

    :sparkles: defining line path with p5

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function s(sk) {
 	sk.setup = () => {
 		sk.createCanvas(window.innerWidth, window.innerHeight).parent('canvasContainer')
 		sk.background(0)
-		// sk.frameRate(60)
+		sk.frameRate(60)
 		sk.angleMode(sk.DEGREES)
 
 		// Create line
@@ -23,42 +23,45 @@ function s(sk) {
 	}
 }
 
+let mouse = P5.createVector()
+window.addEventListener('mousemove', (_event) => {
+	mouse.x = _event.clientX
+	mouse.y = _event.clientY
+})
+
 class Line {
 	constructor(posX, posY) {
 		// Physics values
 		this.pos = P5.createVector(posX, posY)
+		this.oldPos = P5.createVector(posX, posY)
 		this.vel = P5.createVector(0, 0)
 		this.acc = P5.createVector(0, 0)
-		this.angle = null
 
-		this.maxSpeed = 5
+		this.maxSpeed = 2
+		// this.minSpeed = 1
 
 		// Style properties
-		this.strokeWeight = 5
+		this.strokeWeight = 3
 		this.strokeColor = 'white'
-
-		// Noise
-		this.noiseOff = 0
-		this.noiseIncrementation = 0.0005
 	}
 
 	updatePos() {
-		this.noiseOff += this.noiseIncrementation
-		this.angle = (P5.noise(this.noiseOff) - 0.5) * 180
-		// console.log(this.angle)
-		// Convert angle into Vector2
-		this.acc.x = Math.cos(this.angle)
-		this.acc.y = Math.sin(this.angle)
+		this.acc = p5.Vector.sub(mouse, this.pos)
+		this.acc.setMag(0.05)
 
-		// Adding acceleration to velocity then velocity to position
 		this.vel.add(this.acc)
-		this.vel.limit(this.maxSpeed)
+		this.vel.setMag(this.maxSpeed)
 		this.pos.add(this.vel)
 	}
 
 	draw() {
 		P5.strokeWeight(this.strokeWeight)
 		P5.stroke('white')
-		P5.point(this.pos.x, this.pos.y)
+		P5.line(this.oldPos.x, this.oldPos.y, this.pos.x, this.pos.y)
+		this.oldPos = this.pos
+		// P5.background('black')
+		// console.log(this.vel.heading())
+		// P5.line(this.pos.x, this.pos.y, this.pos.x, this.pos.y - 10)
+		console.log('hey')
 	}
 }
